8b5450e48980c8c60db324328ec31d1015b145ff
FTG-003 <siar@member.fsf.org>
2025-03-21 18:44:16 +0100

    V1.0

--- dashboard/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../styles/global.css"; // Assicuriamoci che esista!
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- dashboard/pages/api/update-revision-status.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { file, status } = req.body;
-        res.status(200).json({ message: `Revision for ${file} marked as ${status}.` });
-    } else {
-        res.status(405).json({ message: 'Method Not Allowed' });
-    }
-}
-
